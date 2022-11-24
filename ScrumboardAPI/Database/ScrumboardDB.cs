f2259ee20fe5ffaa32ec92bf561602e8e94017e0
PatrickJessen <patrick-j1@hotmail.dk>
2022-11-24 12:59:29 +0100

    commit

--- ScrumboardAPI/Database/ScrumboardDB.cs
+++ ScrumboardAPI/Database/ScrumboardDB.cs
@@ -29,24 +29,25 @@ namespace ScrumboardAPI.Database
             try
             {
                 Database.Connect();
+                string query = $"INSERT INTO Task (Title, Description, Points, AssignedTo, State, Priority, TaskId) VALUES" +
+                    $" (@title, @description, @points, @assignedTo, @state, @priority, @taskId)";
+                Database.ExecuteQuery(query);
+                Database.command.Parameters.AddWithValue("@title", task.Title);
+                Database.command.Parameters.AddWithValue("@description", task.Description);
+                Database.command.Parameters.AddWithValue("@points", task.Points);
+                Database.command.Parameters.AddWithValue("@assignedTo", task.AssignedTo);
+                Database.command.Parameters.AddWithValue("@state", task.State);
+                Database.command.Parameters.AddWithValue("@priority", task.Priority);
+                Database.command.Parameters.AddWithValue("@taskId", boardId);
+                Database.command.ExecuteScalar();
+                Database.Close();
+                return "Successfully created board";
             }
             catch
             {
+                Database.Close();
                 return "Error 400! Failed to connect to database";
             }
-            string query = $"INSERT INTO Task (Title, Description, Points, AssignedTo, State, Priority, TaskId) VALUES" +
-                $" (@title, @description, @points, @assignedTo, @state, @priority, @taskId)";
-            Database.ExecuteQuery(query);
-            Database.command.Parameters.AddWithValue("@title", task.Title);
-            Database.command.Parameters.AddWithValue("@description", task.Description);
-            Database.command.Parameters.AddWithValue("@points", task.Points);
-            Database.command.Parameters.AddWithValue("@assignedTo", task.AssignedTo);
-            Database.command.Parameters.AddWithValue("@state", task.State);
-            Database.command.Parameters.AddWithValue("@priority", task.Priority);
-            Database.command.Parameters.AddWithValue("@taskId", boardId);
-            Database.command.ExecuteScalar();
-            Database.Close();
-            return "Successfully created board";
         }
 
         public List<Models.Task> GetTasks()
@@ -84,17 +85,18 @@ namespace ScrumboardAPI.Database
             try
             {
                 Database.Connect();
+                string query = $"SELECT * FROM Task WHERE TaskId = @id";
+                Database.ExecuteQuery(query);
+                Database.command.Parameters.AddWithValue("@id", id);
+                Models.Task task = (Models.Task)Database.command.ExecuteScalar();
+                Database.Close();
+                return task;
             }
             catch
             {
+                Database.Close();
                 return null;
             }
-            string query = $"SELECT * FROM Task WHERE TaskId = @id";
-            Database.ExecuteQuery(query);
-            Database.command.Parameters.AddWithValue("@id", id);
-            Models.Task task = (Models.Task)Database.command.ExecuteScalar();
-            Database.Close();
-            return task;
         }
 
         public Board GetBoard()
@@ -103,22 +105,23 @@ namespace ScrumboardAPI.Database
             try
             {
                 Database.Connect();
+                string query = $"SELECT * FROM Board";
+                Database.ExecuteQuery(query);
+                //Database.command.ExecuteScalar();
+                Database.dataReader = Database.command.ExecuteReader();
+                while (Database.dataReader.Read())
+                {
+                    board.States.Add(new BoardState(Database.dataReader["Title"].ToString()));
+                }
+                Database.Close();
+                AddTasksToBoard(board.States, GetTasks());
+                return board;
             }
             catch
             {
+                Database.Close();
                 return null;
             }
-            string query = $"SELECT * FROM Board";
-            Database.ExecuteQuery(query);
-            Database.command.ExecuteScalar();
-            Database.dataReader = Database.command.ExecuteReader();
-            while (Database.dataReader.Read())
-            {
-                board.States.Add(new BoardState(Database.dataReader["Title"].ToString()));
-            }
-            Database.Close();
-            AddTasksToBoard(board.States, GetTasks());
-            return board;
         }
 
         private void AddTasksToBoard(in List<BoardState> states, List<Models.Task> tasks)
@@ -140,18 +143,19 @@ namespace ScrumboardAPI.Database
             try
             {
                 Database.Connect();
+                string query = $"SELECT Id FROM Board WHERE Title = @title";
+                Database.ExecuteQuery(query);
+                Database.command.Parameters.AddWithValue("@title", title);
+                Database.command.ExecuteScalar();
+                int id = Convert.ToInt32(Database.command.ExecuteScalar());
+                Database.Close();
+                return id;
             }
             catch
             {
+                Database.Close();
                 return 0;
             }
-            string query = $"SELECT Id FROM Board WHERE Title = @title";
-            Database.ExecuteQuery(query);
-            Database.command.Parameters.AddWithValue("@title", title);
-            Database.command.ExecuteScalar();
-            int id = Convert.ToInt32(Database.command.ExecuteScalar());
-            Database.Close();
-            return id;
         }
 
         private string TaskStateToString(TaskState state)
